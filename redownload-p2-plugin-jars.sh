877b2e641f2b10e28e54fa2ce680ee170e47dae3
Caspar Krieger <caspar@asparck.com>
2014-11-09 17:47:41 +1100

    Improve P2 re-downloading script
    New repository added to investigate PR #21, which relies on bundles not
    in the core Eclipse updates repo.

--- redownload-p2-plugin-jars.sh
+++ redownload-p2-plugin-jars.sh
@@ -2,23 +2,31 @@
 
 # If, when setting the target platform, Eclipse gets a bunch of errors about being unable to parse
 # manifests and it turns out that each of the jars it complains about is not a valid zip file, you
-# can use this to download the correct jars (the .pack.gz versions of the jars weren't corrupted,
-# in my experience).
+# can use this to download the correct jars in the pack.gz forms (and then unpacking them; the
+# .pack.gz versions of the jars aren't corrupted, in my experience).
+#
+# This script will download the jars from the 3.7.2 update site; you can plug in another update
+# site by editing the script below (we're defaulting to 3.7.2 because at time of writing, that's
+# the lowest version of Eclipse that we support).
 # 
-# Usage: cd to where the pde downloads its plugins, which is typically something like
-#   ~/workspaces/kepler/.metadata/.plugins/org.eclipse.pde.core/.bundle_pool/plugins
-# and then run this script from that directory.
+# Usage: cd to your Eclipse workspace (containing the .metadata directory), and then run this
+# script from there.
 
 set -e
 
 #UPDATE_SITE_BASE_URL=http://download.eclipse.org/eclipse/updates/3.8/R-3.8.2-201301310800/plugins/
-UPDATE_SITE_BASE_URL=http://download.eclipse.org/eclipse/updates/3.7/R-3.7.2-201202080800/plugins/
+#UPDATE_SITE_BASE_URL=http://download.eclipse.org/eclipse/updates/3.7/R-3.7.2-201202080800/plugins/
+UPDATE_SITE_BASE_URL=http://download.eclipse.org/releases/indigo/201202240900/aggregate/plugins/
+
+# PDE downloads its plugins to here
+cd .metadata/.plugins/org.eclipse.pde.core/.bundle_pool/plugins
 
 for f in *.jar; do
 #    echo "Processing ${f}"
+    echo -n .
     if ! unzip -t ${f} 2>&1 > /dev/null ; then
         echo "*** ${f} appears corrupt, redownloading"
-        wget "${UPDATE_SITE_BASE_URL}${f}.pack.gz"
+        curl -Ov "${UPDATE_SITE_BASE_URL}${f}.pack.gz"
         unpack200 -r "${f}.pack.gz" "${f}"
     fi
 done
